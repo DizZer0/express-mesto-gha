ae014127edbf0c83a0979d4889fff52bf45f1457
DizZer0 <hamistry@yandex.ru>
2022-08-05 21:56:02 +0300

    исправить ошибки

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ app.use((req, res, next) => {
 })
 app.use(bodyParser.json());
 app.use('/', userRouter);
-app.use('/', cardRouter)
+app.use('/', cardRouter);
+app.use((req, res, next) => res.status(404).send({message:  "неправильный маршрут" }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
