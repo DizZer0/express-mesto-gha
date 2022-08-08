f27ec8527b0435524ad269b23d98e90369969f14
DizZer0 <hamistry@yandex.ru>
2022-08-08 03:29:31 +0300

    исправить ошибки которые выдал eslint

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {});
-
 app.use((req, res, next) => {
   req.user = {
     _id: '62eab324f8ac69420c13a7f1',
@@ -23,5 +21,7 @@ app.use('/', userRouter);
 app.use('/', cardRouter);
 app.use((req, res, next) => next(new NoDataFound('Неправильный маршрут')));
 app.use(errHandler);
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {});
 
 app.listen(PORT);
