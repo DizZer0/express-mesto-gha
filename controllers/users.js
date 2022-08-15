565545b967a613daa597cb1315283c035b7f8618
DizZer0 <hamistry@yandex.ru>
2022-08-15 13:24:36 +0300

    сделать валидацию запросов, регистрацию и аунтификацию пользователя

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,10 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/user');
 const ValidationError = require('../errors/ValidationError');
 const NoDataFound = require('../errors/NoDataFound');
+const Conflict = require('../errors/Conflict');
 
 module.exports.getAllUsers = (req, res, next) => {
   User.find({})
@@ -26,11 +30,60 @@ module.exports.getByIdUser = (req, res, next) => {
     });
 };
 
-module.exports.createUser = (req, res, next) => {
-  const { name, about, avatar } = req.body;
+module.exports.getMyUser = (req, res, next) => {
+  User.findById(req.user)
+    .then((user) => {
+      if (user) {
+        res.send({ data: user });
+      } else {
+        next(new NoDataFound('Пользователь с таким id не найден'));
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new ValidationError('Невалидный id'));
+      } else {
+        next({ message: 'Произошла ошибка' });
+      }
+    });
+};
 
-  User.create({ name, about, avatar })
+module.exports.createUser = (req, res, next) => {
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
+
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name,
+      about,
+      avatar,
+      email,
+      password: hash,
+    }))
     .then((user) => res.send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new ValidationError('Некорректные данные'));
+      } else if (err.name === 11000) {
+        next(new Conflict('пользователь с таким email уже существует'));
+      } else {
+        next({ message: 'Произошла ошибка' });
+      }
+    });
+};
+
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, 'some-secret-key');
+      res
+        .cookie('jwt', token, {
+          maxAge: 3600000 * 24 * 7,
+        })
+        .send({ message: 'Авторизация успешна' });
+    })
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new ValidationError('Некорректные данные'));
